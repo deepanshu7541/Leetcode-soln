d224c7f61bf81f58b345026f907e77e861983a35
Deepanshu <90151179+deepanshu7541@users.noreply.github.com>
2023-05-28 03:09:42 +0530

    Time: 16 ms (54.75%) | Memory: 17.8 MB (58.17%) - LeetSync

---
+++ asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s1;
+        vector<int> ans;
+        for(int i=0; i<asteroids.size(); i++){
+            //If the asteroid is positiveso push it in stack.
+            if(s1.empty() || asteroids[i] > 0){
+                s1.push(asteroids[i]);
+            }
+            //If the asteroid is negative, then check for 3 cases.
+            // 1. If s1.top is also -ve then they both will not collide and simply push in stack.
+            // 2. if stack top is +ve and has greater value then desroy the asteroid.
+            // 3. If the top stack is +ve and is equal to abs(asteroid[i]), then simply pop that element.
+            else{
+                while(!s1.empty() && s1.top()>0 && s1.top() < abs(asteroids[i])){
+                    s1.pop();
+                }
+                if(!s1.empty() && s1.top() > 0 && s1.top()==abs(asteroids[i])){
+                    s1.pop();
+                }
+                else{
+                    if(s1.empty() || s1.top() < 0){
+                        s1.push(asteroids[i]);
+                    }
+                }
+            }
+        }
+        while(!s1.empty()){
+            ans.push_back(s1.top());
+            s1.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
